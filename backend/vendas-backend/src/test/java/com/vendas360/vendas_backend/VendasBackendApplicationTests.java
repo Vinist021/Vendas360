d2b835c6f210212b34409dd25a609dfc705f0393
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-24 11:03:07 -0300

    build: configura o maven no projeto e adiciona dependências

---
+++ backend/vendas-backend/src/test/java/com/vendas360/vendas_backend/VendasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas360.vendas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
