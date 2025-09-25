fc4fa82fd8643cb2f82e8f6fd412f76dd78c70fd
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-25 16:22:25 -0300

    feat: controlador retorna ResponseEntity nos endpoints e inclui mensagem no delete

--- backend/vendas-backend/src/main/java/com/vendas360/vendas_backend/controllers/SellerController.java
+++ backend/vendas-backend/src/main/java/com/vendas360/vendas_backend/controllers/SellerController.java
@@ -1,8 +1,10 @@
 package com.vendas360.vendas_backend.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.vendas360.vendas_backend.models.Seller;
 import com.vendas360.vendas_backend.services.SellerService;
@@ -25,28 +28,43 @@ public class SellerController {
     SellerService sellerService;
 
     @GetMapping()
-    public List<Seller> getSellers() {
-        return sellerService.getAll();
+    public ResponseEntity<List<Seller>> getSellers() {
+        return ResponseEntity.ok(sellerService.getAll());
     }
 
     @GetMapping("{id}")
-    public Seller getSeller(@PathVariable long id) {
-        return sellerService.getById(id);
+    public ResponseEntity<Seller> getSeller(@PathVariable long id) {
+        Seller seller = sellerService.getById(id);
+        return ResponseEntity.ok(seller);
     }
 
     @PostMapping()
-    public Seller saveSeller(@RequestBody Seller seller) {
-        return sellerService.save(seller);
+    public ResponseEntity<Seller> saveSeller(@RequestBody Seller seller) {
+
+        Seller newSeller = sellerService.save(seller);
+
+        URI location = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(seller.getId())
+        .toUri();
+
+        return ResponseEntity.created(location).body(newSeller); 
     }
 
     @PutMapping("{id}")
-    public void updateSeller(@PathVariable long id, @RequestBody Seller seller ){
-        sellerService.update(id, seller);
+    public ResponseEntity<Seller> updateSeller(@PathVariable long id, @RequestBody Seller seller ){
+        Seller updatedSeller = sellerService.update(id, seller);
+        return ResponseEntity.ok(updatedSeller);
     }
 
     @DeleteMapping("{id}")
-    public void deleteSeller(@PathVariable long id) {
-        sellerService.deleteById(id);
-    }
+    public ResponseEntity<Void> deleteSeller(@PathVariable long id) {
 
+        sellerService.deleteById(id);
+
+        return ResponseEntity.noContent()
+                    .header("message", "Vendedor excluído com sucesso")
+                    .build();
+    }
 }
