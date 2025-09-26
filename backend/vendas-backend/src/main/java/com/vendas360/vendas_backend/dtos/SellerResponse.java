9eb840957d6d5f44343f295adc1e9ac19a274689
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-26 13:00:04 -0300

    feat: adiciona DTO SellerResponse

---
+++ backend/vendas-backend/src/main/java/com/vendas360/vendas_backend/dtos/SellerResponse.java
@@ -0,0 +1,53 @@
+package com.vendas360.vendas_backend.dtos;
+
+public class SellerResponse {
+    
+    private Long id;
+    private String name;
+    private Double salary;
+    private Double bonus;
+    private Integer gender;
+
+    public SellerResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(Double bonus) {
+        this.bonus = bonus;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+}
