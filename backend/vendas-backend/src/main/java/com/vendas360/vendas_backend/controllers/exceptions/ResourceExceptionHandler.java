db18fcddad0a81b10816da16e37f771e1a3edca2
Vinicius Teixeira <vinist.002@gmail.com>
2025-09-28 14:32:22 -0300

    refactor: altera retorno do tratamento de MethodArgumentNotValidException para StandardError

--- backend/vendas-backend/src/main/java/com/vendas360/vendas_backend/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/vendas-backend/src/main/java/com/vendas360/vendas_backend/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.vendas360.vendas_backend.controllers.exceptions;
 
+import java.time.Instant;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,7 +14,14 @@ import jakarta.servlet.http.HttpServletRequest;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> validationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation error");
+    public ResponseEntity<StandardError> validationException(MethodArgumentNotValidException exception, HttpServletRequest request) {
+        StandardError error = new StandardError();
+        error.setError("Validation error");
+        error.setMessage(exception.getMessage());
+        error.setPath(request.getRequestURI());
+        error.setTimeStamp(Instant.now());
+        error.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }    
 }
