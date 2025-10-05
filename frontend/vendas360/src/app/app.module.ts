e35420e8929f3d339c792b89ed4aa6b0baabfd05
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-05 14:08:58 -0300

    feat: define estrutura inicial do formulário de cadastro

--- frontend/vendas360/src/app/app.module.ts
+++ frontend/vendas360/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SellerFormComponent } from './components/seller-form/seller-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SellerFormComponent,
   ],
   imports: [
     BrowserModule,
