f540f1bfef731285945efa100fa214f38b49accf
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-08 15:28:57 -0300

    feat: integra tabela com serviço reativo para atualização automática

--- frontend/vendas360/src/app/components/seller-table/seller-table.component.ts
+++ frontend/vendas360/src/app/components/seller-table/seller-table.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Seller } from 'src/app/interfaces/Seller';
+import { SellerSharedService } from 'src/app/services/seller-shared.service';
 
 @Component({
   selector: 'app-seller-table',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerTableComponent implements OnInit {
 
-  constructor() { }
+  sellers$: Observable<Seller[]>;
+
+  constructor(private sellerService: SellerSharedService) {
+    this.sellers$ = this.sellerService.sellers$;
+   }
 
   ngOnInit(): void {
+    this.sellerService.getSellers().subscribe();
   }
 
 }
