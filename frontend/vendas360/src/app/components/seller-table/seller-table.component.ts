3599f412b7555d2e10bbae3f889844d0350e320b
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-08 15:50:41 -0300

    feat: implementa funcionalidade de exclusão de vendedor

--- frontend/vendas360/src/app/components/seller-table/seller-table.component.ts
+++ frontend/vendas360/src/app/components/seller-table/seller-table.component.ts
@@ -14,10 +14,23 @@ export class SellerTableComponent implements OnInit {
 
   constructor(private sellerService: SellerSharedService) {
     this.sellers$ = this.sellerService.sellers$;
-   }
+  }
 
   ngOnInit(): void {
     this.sellerService.getSellers().subscribe();
+  }
+
+  deleteSeller(id: number): void {
+    if (confirm('Tem certeza que deseja excluir este vendedor?')) {
+      this.sellerService.deleteSeller(id).subscribe({
+        next: () => {
+          console.log(`Vendedor ID ${id} excluído com sucesso`);
+        },
+        error: err => {
+          console.error('Erro ao excluir vendedor', err);
+        }
+      });
+    }
   }
 
 }
